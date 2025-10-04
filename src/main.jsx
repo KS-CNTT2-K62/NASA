67fcdb3b0b23cfc6b05e5b30fe09a7ae4afd4744
nqson <59283327+nguyenson2003@users.noreply.github.com>
2025-10-04 12:21:47 +0700

    add config deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' // Thêm dòng này
+import { BrowserRouter, HashRouter } from 'react-router-dom' // Thêm dòng này
 import './index.css'
 import App from './App.jsx'
+const basename = "/NASA/"; 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
